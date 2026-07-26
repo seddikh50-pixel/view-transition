ed47d585f5e5137fe235c2860a3803e39fbfba33
seddik <seddikh50@gmail.com>
2026-07-26 15:51:02 +0100

    dsd

--- app/Machine/page.tsx
+++ app/Machine/page.tsx
@@ -8,12 +8,12 @@ export default function Gateway() {
      
       }}
     >
-        <Image
+        {/* <Image
             src="/machine.jpg"
             alt=""
             fill
             className="object-cover"
-          />
+          /> */}
               <h1 className=" absolute bottom-15 text-9xl font-black left-20 z-10  text-white text-shadow-lg/30 ">Machine</h1>
     </main>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ export default function Gateway() {
     <main
       className="w-full h-screen relative"
     >
-        <Image
+        {/* <Image
             src="/fares.jpg"
             alt="d"
             fill
             className="object-cover"
-          />
+          /> */}
           <h1 className=" absolute bottom-15 text-9xl font-black left-20 z-10  text-white text-shadow-lg/30 ">Fares</h1>
     </main>
   );
